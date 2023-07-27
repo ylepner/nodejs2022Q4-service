aea4a7b9332d109c2209576e68eb0f13652b5d46
Julia Lepner <ylepner@gmail.com>
2023-07-27 22:36:36 +0200

    refactor: replace file

--- src/models.ts
+++ src/models.ts
@@ -1,17 +1,7 @@
-
-
 export interface Artist {
   id: string; // uuid v4
   name: string;
   grammy: boolean;
-}
-
-export interface Track {
-  id: string; // uuid v4
-  name: string;
-  artistId: string | null; // refers to Artist
-  albumId: string | null; // refers to Album
-  duration: number; // integer number
 }
 
 export interface Album {
