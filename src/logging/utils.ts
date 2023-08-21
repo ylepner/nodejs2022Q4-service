4ff6454589da3bc346abcd981ecb4e5924c17ec5
Julia Lepner <ylepner@gmail.com>
2023-08-21 23:29:35 +0200

    feat: get log name

--- src/logging/utils.ts
+++ src/logging/utils.ts
@@ -1,12 +1,29 @@
 import { join } from 'path';
-import { appendFile, stat } from 'fs/promises';
+import { appendFile, stat, mkdir } from 'fs/promises';
 
 const sizeLimitInBytes = (Number(process.env.LOG_SIZE_KB) || 10) * 1024;
 let count = 0;
 let currentFileSizeInBytes = 0;
 
+let checked = false;
+let date = new Date();
+const logFileName = getLogName();
 export async function writeToFile(message: string) {
-  const path = join(process.cwd(), '', `/logs(${count}).log`);
+  const folder = join(process.cwd(), 'logs');
+  if (!checked) {
+    try {
+      console.log(`Creating folder ${folder}`)
+      await mkdir(folder, {
+        recursive: true,
+      });
+    } catch (e) {
+      console.error(`Cannot create folder ${folder}`);
+      console.error(e);
+    }
+
+    checked = true;
+  }
+  const path = join(folder, logFileName);
   // const size = await checkSize(path);
   // if (size) {
   //   if (currentFileSizeInBytes + size <= sizeLimitInBytes) {
@@ -41,4 +58,16 @@ async function checkSize(path: string) {
   } catch {
     return null;
   }
+}
+
+function getLogName() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+  const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+  const filename = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}.log`;
+  return filename;
 }
